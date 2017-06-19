d146c16b7b5341598ba41cc643e01920087e8954
Ganesh Iyer <lastlegion@gmail.com>
2017-06-19 13:49:59 -0400

    modifying run.sh to use environment variables

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+if [ -z "$QUIP_JOBS" ]; then
+	sed -i 's/\@QUIP_JOBS/\$QUIP_JOBS/g' /tmp/DynamicServices/configs/config.json 
+	sed -i 's/\@QUIP_OSS/\$QUIP_OSS/g' /tmp/DynamicServices/configs/config.json
+	sed -i 's/\@QUIP_DATA/\$QUIP_DATA/g' /tmp/DynamicServices/configs/config.json
+	sed -i 's/\@QUIP_LOADER/\$QUIP_LOADER/g' /tmp/DynamicServices/configs/config.json
+fi
+	
 nohup /tmp/DynamicServices/./dynamicservices /tmp/DynamicServices/configs/config.json > /tmp/DynamicServices/configs/dynamicservices.log 2>&1 &
 while true; do sleep 1000; done
 
