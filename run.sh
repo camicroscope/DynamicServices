10e74c9245158f3b24bfd178ac597a4ec8415d90
Ganesh Iyer <lastlegion@gmail.com>
2017-06-21 13:34:53 -0400

    removed if statement for running sed

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ sleep 60
 apikey=$(python createUser.py dynamic@quip)
 sed -i -e "s/ffffffff-ffff-ffff-ffff-ffffffffffff/$apikey/g" /tmp/DynamicServices/configs/sample_apikey.json
 
-if [ -z "$QUIP_JOBS" ]; then
-	sed -i "s@QUIP_JOBS@$QUIP_JOBS@g" /tmp/DynamicServices/configs/config.json 
-	sed -i "s@QUIP_OSS@$QUIP_OSS@g" /tmp/DynamicServices/configs/config.json
-	sed -i "s@QUIP_DATA@$QUIP_BINDAAS@g" /tmp/DynamicServices/configs/config.json
-	sed -i "s@QUIP_LOADER@$QUIP_LOADER@g" /tmp/DynamicServices/configs/config.json
-fi
+
+sed -i "s@QUIP_JOBS@$QUIP_JOBS@g" /tmp/DynamicServices/configs/config.json 
+sed -i "s@QUIP_OSS@$QUIP_OSS@g" /tmp/DynamicServices/configs/config.json
+sed -i "s@QUIP_DATA@$QUIP_BINDAAS@g" /tmp/DynamicServices/configs/config.json
+sed -i "s@QUIP_LOADER@$QUIP_LOADER@g" /tmp/DynamicServices/configs/config.json
+
 	
 nohup /tmp/DynamicServices/./dynamicservices /tmp/DynamicServices/configs/config.json > /tmp/DynamicServices/configs/dynamicservices.log 2>&1 &
 while true; do sleep 1000; done
