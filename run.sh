b22d2173faf859eb24a0696af738774eced3d6aa
Ganesh Iyer <lastlegion@gmail.com>
2017-08-02 10:55:40 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ apikey=$(python createUser.py dynamic@quip)
 sed -i -e "s/ffffffff-ffff-ffff-ffff-ffffffffffff/$apikey/g" /tmp/DynamicServices/sample_apikey.json
 
 
-sed -i "s@camicroscope-jobs@$camicroscope-jobs@g" /tmp/DynamicServices/configs/config.json 
-sed -i "s@camicroscope-oss@$camicroscope-oss@g" /tmp/DynamicServices/configs/config.json
-sed -i "s@camicroscope-bindaas@$camicroscope-bindaas@g" /tmp/DynamicServices/configs/config.json
-sed -i "s@camicroscope-uaimloader@$camicroscope-uaimloader@g" /tmp/DynamicServices/configs/config.json
+#sed -i "s@camicroscope-jobs@$camicroscope-jobs@g" /tmp/DynamicServices/configs/config.json 
+#sed -i "s@camicroscope-oss@$camicroscope-oss@g" /tmp/DynamicServices/configs/config.json
+#sed -i "s@camicroscope-bindaas@$camicroscope-bindaas@g" /tmp/DynamicServices/configs/config.json
+#sed -i "s@camicroscope-uaimloader@$camicroscope-uaimloader@g" /tmp/DynamicServices/configs/config.json
 	
 nohup /tmp/DynamicServices/./dynamicservices /tmp/DynamicServices/configs/config.json > /tmp/DynamicServices/configs/dynamicservices.log 2>&1 &
 while true; do sleep 1000; done
